66cb4d06b2f48aaecab5d446f2d57eafcef469c7
100rabhcsmc <saurabhchavan052@gmail.com>
2023-01-24 22:33:58 +0530

    SwiftUI Bootcamp

---
+++ SwiftUIBootcamp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUIBootcamp
+//
+//  Created by Saurabh Chavan on 24/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World !")
+            .font(.title)
+            .fontWeight(.medium)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Saurabh Chavan on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
