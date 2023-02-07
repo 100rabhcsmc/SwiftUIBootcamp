9213e8a4e168704c3b9bae4aa0888f00891ad944
100rabhcsmc <saurabhchavan052@gmail.com>
2023-02-08 00:11:36 +0530

    Frames and Alignments

--- SwiftUIBootcamp/FrameBootcamp.swift
+++ SwiftUIBootcamp/FrameBootcamp.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct FrameBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("Hello SwiftUI")
+                .background(Color.red)
+                .frame(height: 100,alignment: .top)
+                .background(Color.orange)
+                .frame(width: 150)
+                .background(Color.purple)
+                .frame(maxWidth: .infinity,alignment: .trailing)
+                .background(Color.pink)
+                .frame(height: 400)
+                .background(Color.green)
+                .frame(maxHeight: .infinity,alignment: .top)
+                .background(Color.yellow)
+            
+        }
     }
 }
 
