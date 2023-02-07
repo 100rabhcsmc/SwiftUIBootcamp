56b4dbe648eb751e2b6f9617b6133ca303fc4227
100rabhcsmc <saurabhchavan052@gmail.com>
2023-02-07 23:12:33 +0530

    frame introduction

---
+++ SwiftUIBootcamp/FrameBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  FrameBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Saurabh Chavan on 07/02/23.
+//
+
+import SwiftUI
+
+struct FrameBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameBootcamp()
+    }
+}
